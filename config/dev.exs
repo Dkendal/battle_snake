3ad6d8cf44ac95cf692502f3694f200ab890c490
Dylan Kendal <dylankendal@gmail.com>
2016-07-25 12:59:38 -0700

    start server with PORT

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :battle_snake_server, BattleSnakeServer.Endpoint,
-  http: [port: 4000],
+  http: [port: {:system, "PORT"}],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
