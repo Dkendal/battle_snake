976d7e1eb0d3affeab82549fdee1e758afd174fb
Dylan Kendal <dylankendal@gmail.com>
2016-07-26 00:01:26 -0700

    add update
    and refactor some stuff

--- web/controllers/game_controller.ex
+++ web/controllers/game_controller.ex
@@ -14,37 +14,78 @@ defmodule BattleSnakeServer.GameController do
   end
 
   def create(conn, %{"game" => params}) do
-    id = Enum.join(Tuple.to_list(:erlang.now), "-")
-
-    world = struct World, game_params(params)
-
-    game = %Game{id: id, state: world}
-
-    :ok = :mnesia.dirty_write(Game, Game.record(game))
+    world = struct(World)
+    params = create_params(params)
+    game = %Game{params | id: new_id, state: world}
+    save(game)
 
     redirect(conn, to: game_path(conn, :show, game))
   end
 
   def show(conn, %{"id" => id}) do
-    [game] = :mnesia.dirty_read Game, id
-    game = Game.load(game)
+    game = load(id)
 
     render(conn, "show.html", game: game)
   end
 
   def edit(conn, %{"id" => id}) do
+    game = load(id)
+
+    render(conn, "edit.html", game: game)
   end
 
-  def update(conn, %{"id" => id, "game" => game_params}) do
+  def update(conn, %{"id" => id, "game" => params}) do
+    game = load(id)
+
+    params = update_params(params)
+
+    callback = fn (_, new, old) ->
+      new || old
+    end
+
+    game = Map.merge(game, params, callback)
+
+    save(game)
+
+    redirect(conn, to: game_path(conn, :show, game))
   end
 
   def delete(conn, %{"id" => id}) do
   end
 
-  def game_params(params) do
-    %{
+  def load(id) do
+    read = fn ->
+      :mnesia.read Game, id
+    end
+
+    {:atomic, [game]} = :mnesia.transaction read
+
+    Game.load(game)
+  end
+
+  def save(game) do
+    write = fn ->
+      :mnesia.write(Game.record(game))
+    end
+
+    {:atomic, :ok} = :mnesia.transaction write
+
+    game
+  end
+
+  def create_params(params) do
+    %Game{
       width: String.to_integer(params["width"]),
       height: String.to_integer(params["height"]),
     }
+  end
+
+  def update_params(_params) do
+    %Game{
+    }
+  end
+
+  def new_id do
+    Enum.join(Tuple.to_list(:erlang.now), "-")
   end
 end
