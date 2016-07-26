2cb4c2e42730bdc6135c45156f48b910a9138bf3
Dylan Kendal <dylan@stembolt.com>
2016-07-25 21:36:50 -0700

    Add a script to create db
    creates a simple mnesia schema and the game table, which has an id and
    state

--- test/models/game_test.exs
+++ test/models/game_test.exs
@@ -2,4 +2,17 @@ defmodule BattleSnakeServer.GameTest do
   use ExUnit.Case, async: true
 
   alias BattleSnakeServer.Game
+
+  describe "#table" do
+    test "returns the decleration for mnesia" do
+      assert Game.table == [attributes: [:id, :state]]
+    end
+  end
+
+  describe "#record" do
+    test "converts a struct into a record" do
+      game = %Game{state: %{}, id: 1}
+      assert Game.record(game) == {Game, 1, %{}}
+    end
+  end
 end
