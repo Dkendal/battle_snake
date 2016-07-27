a225f1b3e3343d07cb818dd910d77469f1179fa3
Dylan Kendal <dylankendal@gmail.com>
2016-07-26 18:51:07 -0700

    add game changeset

--- test/models/game_test.exs
+++ test/models/game_test.exs
@@ -5,21 +5,27 @@ defmodule BattleSnakeServer.GameTest do
 
   describe "#table" do
     test "returns the decleration for mnesia" do
-      assert Game.table == [attributes: [:id, :state, :width, :height, :snakes]]
+      assert Game.table == [attributes: [
+        :id,
+        :height,
+        :snakes,
+        :state,
+        :width,
+      ]]
     end
   end
 
   describe "#record" do
     test "converts a struct into a record" do
       game = %Game{state: %{}, id: 1, width: 20, height: 40, snakes: []}
-      assert Game.record(game) == {Game, 1, %{}, 20, 40, []}
+      assert Game.record(game) == {Game, 1, 40, [], %{}, 20}
     end
   end
 
   describe "#load" do
     test "converts a record to a struct" do
-      record = {Game, 1, %{}, 20, 40}
-      assert Game.load(record) == %Game{id: 1, state: %{}, width: 20, height: 40}
+      record = {Game, 1, 40, [], %{}, 20}
+      assert Game.load(record) == %Game{id: 1, state: %{}, width: 20, height: 40, snakes: []}
     end
   end
 end
