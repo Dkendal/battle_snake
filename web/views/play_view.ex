da7a9f95a4b007fee1e2feeec66f7f2cdb115581
Dylan Kendal <dylankendal@gmail.com>
2016-07-28 18:27:12 -0700

    SVG rendering of the board!

--- web/views/play_view.ex
+++ web/views/play_view.ex
@@ -1,3 +1,23 @@
 defmodule BattleSnakeServer.PlayView do
   use BattleSnakeServer.Web, :view
+
+  def render("square.html", %{state: state, y: y, x: x}) do
+    square = state.map[x][y]
+
+    square_state = square["state"]
+
+    fill = case square_state do
+      "head" -> "red"
+
+      "body" -> "blue"
+
+      "food" -> "green"
+
+      _ -> "black"
+    end
+
+    style = "fill: #{fill};"
+
+    content_tag(:rect, "", x: x, y: y, width: 1, height: 1, style: style)
+  end
 end
