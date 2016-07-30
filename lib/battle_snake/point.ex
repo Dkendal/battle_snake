1afe8e44aeff5887e25069cfd993937ba60f90e1
Dylan Kendal <dylan@stembolt.com>
2016-07-30 16:19:21 -0700

    Reimplement Snake.move

--- lib/battle_snake/point.ex
+++ lib/battle_snake/point.ex
@@ -1,3 +1,17 @@
 defmodule BattleSnake.Point do
   defstruct [:x, :y]
+
+  def sub(a, b) do
+    %__MODULE__{
+      y: a.y - b.y,
+      x: a.x - b.x,
+    }
+  end
+
+  def add(a, b) do
+    %__MODULE__{
+      y: a.y + b.y,
+      x: a.x + b.x,
+    }
+  end
 end
