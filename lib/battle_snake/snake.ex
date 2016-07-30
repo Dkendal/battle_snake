15758a117da8c43907f0580f2fa043a950200256
Dylan Kendal <dylankendal@gmail.com>
2016-07-30 15:39:02 -0700

    remove Snake.new

--- lib/battle_snake/snake.ex
+++ lib/battle_snake/snake.ex
@@ -13,23 +13,6 @@ defmodule BattleSnake.Snake do
     Enum.member? stream, head
   end
 
-  def new(params, rows, cols) do
-    starting_pos = [:rand.uniform(rows), :rand.uniform(cols)]
-
-    coords = (for _ <- 0..3, do: starting_pos)
-
-    default = %{
-      "color" => "",
-      "coords" => coords,
-      "head_url" => "",
-      "name" => "",
-      "taunt" => "",
-      "url" => "",
-    }
-
-    Dict.merge default, params
-  end
-
   def len(snake) do
     length snake["coords"]
   end
