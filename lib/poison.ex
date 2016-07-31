2003cdce5f85f6631d39ceda6ab7ea1b8f1c4ba9
Dylan Kendal <dylan@stembolt.com>
2016-07-31 14:06:12 -0700

    serialize snakes, food, and the turn

--- lib/poison.ex
+++ lib/poison.ex
@@ -1,5 +1,21 @@
 defimpl Poison.Encoder, for: BattleSnake.World do
-  def encode(world, _) do
-    ""
+  def encode(world, opts) do
+    attrs = [
+      :food,
+      :turn,
+      :snakes,
+    ]
+
+    board = []
+
+    map = %{
+      board: board
+    }
+
+    map = world
+    |> Map.take(attrs)
+    |> Dict.merge(map)
+
+    Poison.encode!(map, opts)
   end
 end
--- test/poison_test.exs
+++ test/poison_test.exs
@@ -2,10 +2,27 @@ defmodule PoisonTest do
   use ExUnit.Case, async: true
 
   describe "BattleSnake.World" do
+    alias BattleSnake.{World, Point}
+
     test "is encoded" do
-      world = %BattleSnake.World{}
-      encoding = Poison.encode(world, [])
-      assert {:ok, ""} == encoding
+      world = %BattleSnake.World{
+        turn: 0,
+        food: [],
+        snakes: [],
+      }
+
+      actual = world
+      |> Poison.encode!
+      |> Poison.decode!
+
+      expected = %{
+        "board" => [],
+        "food" => [],
+        "snakes" => [],
+        "turn" => 0,
+      }
+
+      assert expected == actual
     end
   end
 end
