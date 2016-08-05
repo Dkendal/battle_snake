9566de8c476b5c7c7e5fff25fcaa73ec76a6d384
Dylan Kendal <dylankendal@gmail.com>
2016-08-05 00:00:37 -0700

    Start game causes tick function to continously update state

--- test/battle_snake/game_server_test.exs
+++ test/battle_snake/game_server_test.exs
@@ -6,13 +6,31 @@ defmodule BattleSnake.GameServerTest do
 
   setup do
     world = %World{}
-    {:ok, pid} = GameServer.start_link(world)
+
+    this = self
+
+    f = fn world ->
+      world = update_in world.turn, & &1+1
+      send this, {:turn, world.turn}
+      world
+    end
+
+    opts = [delay: 0]
+
+    {:ok, pid} = GameServer.start_link({world, f, opts})
+
     %{pid: pid}
   end
 
   describe ".start_game" do
-    test "starts the game", %{pid: pid} do
+    test "returns the world", %{pid: pid} do
       %World{} = GameServer.start_game(pid)
+    end
+
+    test "calls the tick function repeatably", %{pid: pid} do
+      start(pid)
+      assert_receive {:turn, 1}, 100
+      assert_receive {:turn, 2}, 100
     end
   end
 
@@ -20,5 +38,9 @@ defmodule BattleSnake.GameServerTest do
   end
 
   describe ".stop_game" do
+  end
+
+  def start(pid) do
+    GameServer.start_game(pid)
   end
 end
