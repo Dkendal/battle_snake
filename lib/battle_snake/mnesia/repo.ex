441175b3c34d3044a019281584d186b7a098b70d
Dylan Kendal <dylankendal@gmail.com>
2016-08-07 21:22:43 -0700

    move all mnesia related code to it's own file

---
+++ lib/battle_snake/mnesia/repo.ex
@@ -0,0 +1,45 @@
+defmodule BattleSnake.Mnesia.Repo do
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+
+      def table, do: [attributes: fields]
+
+      def fields, do: __schema__(:fields)
+
+      def record(game) do
+        get = &Map.get(game, &1)
+        attrs = Enum.map(fields, get)
+        List.to_tuple [__MODULE__ |attrs]
+      end
+
+      def load(record) do
+        [__MODULE__ |attrs] = Tuple.to_list(record)
+        attrs = Enum.zip(fields, attrs)
+        struct(__MODULE__, attrs)
+      end
+
+      def all do
+        fn ->
+          :qlc.e(:mnesia.table __MODULE__)
+        end
+        |> :mnesia.async_dirty()
+        |> Enum.map(&load/1)
+      end
+
+      def last do
+        load :mnesia.last(__MODULE__)
+      end
+
+      def get(id) do
+        read = fn ->
+          :mnesia.read __MODULE__, id
+        end
+
+        {:atomic, [record]} = :mnesia.transaction read
+
+        load(record)
+      end
+    end
+  end
+end
--- web/models/game.ex
+++ web/models/game.ex
@@ -17,45 +17,6 @@ defmodule BattleSnakeServer.Game do
     field :max_food, :integer, default: 1
   end
 
-  def fields, do: __schema__(:fields)
-
-  def table, do: [attributes: fields]
-
-  def record(game) do
-    get = &Map.get(game, &1)
-    attrs = Enum.map(fields, get)
-    List.to_tuple [__MODULE__ |attrs]
-  end
-
-  def load(record) do
-    [__MODULE__ |attrs] = Tuple.to_list(record)
-    attrs = Enum.zip(fields, attrs)
-    struct(__MODULE__, attrs)
-  end
-
-
-  def all do
-    fn ->
-      :qlc.e(:mnesia.table __MODULE__)
-    end
-    |> :mnesia.async_dirty()
-    |> Enum.map(&load/1)
-  end
-
-  def last do
-    load :mnesia.last(__MODULE__)
-  end
-
-  def get(id) do
-    read = fn ->
-      :mnesia.read __MODULE__, id
-    end
-
-    {:atomic, [game]} = :mnesia.transaction read
-
-    load(game)
-  end
-
   def reset_snake(%World{} = world, %Snake{} = snake) do
     coords = new_coords(world)
     %{snake| coords: coords}
