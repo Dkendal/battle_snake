22dc38e496cceb2730940bad44f517a021766a48
Dylan Kendal <dylankendal@gmail.com>
2016-08-08 22:53:21 -0700

    Bump elixir version to 1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BattleSnakeServer.Mixfile do
   def project do
     [app: :battle_snake_server,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
