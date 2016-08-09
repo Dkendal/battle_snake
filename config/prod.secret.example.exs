34f7a5ec5611de6fd954b1c6deaba8d450616d1a
Dylan Kendal <dylan@stembolt.com>
2016-08-08 22:40:05 -0700

    Add an example prod secret file

---
+++ config/prod.secret.example.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# Rename this file to prod.secret.example
+# generate a new secret with
+# `mix phoenix.gen.secret`
+config :battle_snake_server, BattleSnakeServer.Endpoint,
+  secret_key_base: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
