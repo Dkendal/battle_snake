55304f9eb42c1925b6f03b8435b6e14ffcbc167d
Dylan Kendal <dylankendal@gmail.com>
2016-08-09 22:26:05 -0700

    remove unused methods

--- lib/battle_snake/board.ex
+++ lib/battle_snake/board.ex
@@ -1,12 +1,4 @@
 defmodule BattleSnake.Board do
-  def height(board) do
-    length board
-  end
-
-  def width([row | _]) do
-    length row
-  end
-
   def empty, do: %{"state" => "empty"}
   def food, do: %{"state" => "food"}
 end
