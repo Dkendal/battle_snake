94584b3b0f024a54cf5ee9038840344485ab7d77
Dylan Kendal <dylankendal@gmail.com>
2017-01-14 14:21:09 -0800

    Move.all handles dead tasks
    When a task dies Move will assume the default move.

--- lib/battle_snake/move.ex
+++ lib/battle_snake/move.ex
@@ -14,27 +14,17 @@ defmodule BattleSnake.Move do
   @doc "collect all moves for living snakes"
   @spec all(list(Snake.t), (Snake.t -> Move.t)) :: list(Snake.t)
   def all(snakes, move_fn, timeout \\ 5000) do
+    {:ok, sup_pid} = Task.Supervisor.start_link
+
     snakes
     |> Enum.map(fn snake ->
-      Task.async(fn ->
+      Task.Supervisor.async_nolink(sup_pid, fn ->
         move_fn.(snake)
       end)
     end)
     |> Task.yield_many(timeout)
-    |> Stream.map(fn {task, result} ->
-      # Kill tasks that exceeded the timeout.
-      result || Task.shutdown(task, :brutal_kill)
-    end)
-    |> Stream.map(fn
-      nil ->
-        # Task timed out
-        default_move
-      {:exit, _} ->
-        # Task died
-        default_move
-      {:ok, value} ->
-        value
-    end)
+    |> Stream.map(&task_cleanup/1)
+    |> Stream.map(&task_value/1)
     |> Enum.to_list
   end
 
@@ -42,5 +32,25 @@ defmodule BattleSnake.Move do
     %Move{
       move: "up"
     }
+  end
+
+  defp task_value(value) do
+    case(value) do
+      nil ->
+        # Timeout
+        default_move
+
+      {:exit, _} ->
+        # Process died
+        default_move
+
+      {:ok, v} ->
+        v
+    end
+  end
+
+  defp task_cleanup({task, result}) do
+    # Kill tasks that exceeded the timeout.
+    result || Task.shutdown(task, :brutal_kill)
   end
 end
