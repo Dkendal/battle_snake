223861d45b17158e8df3652bcb6ed1bf8337bce4
Dylan Kendal <dylan@stembolt.com>
2017-01-14 15:44:30 -0800

    Added a simple ruby example snake
    A dumb sinatra based snake that spins in circles.

---
+++ examples/ruby_snake/ruby_snake.rb
@@ -0,0 +1,21 @@
+require "json"
+
+class RubySnake < Sinatra::Base
+  def initialize(*)
+    @@i ||= 0
+    super
+  end
+
+  def move
+    @@i = (@@i + 1) % 4
+    %w(up left down right)[@@i]
+  end
+
+  post "/start" do
+    {}.to_json
+  end
+
+  post "/move" do
+    {move: move()}.to_json
+  end
+end
