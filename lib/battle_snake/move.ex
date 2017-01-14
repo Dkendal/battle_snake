be0cb848be1c36c29b88bb6a47bea498147389fa
Dylan Kendal <dylan@stembolt.com>
2017-01-14 15:08:43 -0800

    Bake snake into move value
    move values have the value of the snake as an attribute so that we can
    figure out what move belongs to what snake.

--- lib/battle_snake/move.ex
+++ lib/battle_snake/move.ex
@@ -2,55 +2,57 @@ defmodule BattleSnake.Move do
   alias __MODULE__
   alias BattleSnake.Snake
 
-  defstruct [:move, :taunt]
+  defstruct [:move, :taunt, :snake]
 
   @type direction :: String.t
 
   @type t :: %__MODULE__{
     move: direction,
+    snake: Snake.t,
     taunt: String.t,
   }
 
-  @doc "collect all moves for living snakes"
+  @doc """
+  Collects all moves for all living snakes for world.
+
+  For each snake in world.snakes, a linked task is started, which in turn
+  starts an unlinked task when performs the actual request.
+
+  When the requesting task times-out it is killed, and the default move is
+  reported back to the supervising task which is then aggregated with the
+  other results.
+  """
+  @shortdoc "collect all moves for living snakes"
   @spec all(list(Snake.t), (Snake.t -> Move.t)) :: list(Snake.t)
-  def all(snakes, move_fn, timeout \\ 5000) do
-    {:ok, sup_pid} = Task.Supervisor.start_link
+  def all(snakes, request_fun, timeout \\ 5000) do
+    do_task =
+    fn (snake) ->
+      {:ok, sup_pid} = Task.Supervisor.start_link()
+
+      task = Task.Supervisor.async_nolink(
+        sup_pid,
+        fn -> request_fun.(snake) end)
+
+      move =
+        case Task.yield(task, timeout) || Task.shutdown(task) do
+          {:ok, move} ->
+            move
+          nil ->
+            default_move
+        end
+
+      # identify what snake this move belongs to.
+      put_in(move.snake, snake)
+    end
 
     snakes
-    |> Enum.map(fn snake ->
-      Task.Supervisor.async_nolink(sup_pid, fn ->
-        move_fn.(snake)
-      end)
-    end)
-    |> Task.yield_many(timeout)
-    |> Stream.map(&task_cleanup/1)
-    |> Stream.map(&task_value/1)
-    |> Enum.to_list
+    |> Task.async_stream(do_task)
+    |> Enum.into([], fn {:ok, value} -> value end)
   end
 
   defp default_move do
     %Move{
       move: "up"
     }
-  end
-
-  defp task_value(value) do
-    case(value) do
-      nil ->
-        # Timeout
-        default_move
-
-      {:exit, _} ->
-        # Process died
-        default_move
-
-      {:ok, v} ->
-        v
-    end
-  end
-
-  defp task_cleanup({task, result}) do
-    # Kill tasks that exceeded the timeout.
-    result || Task.shutdown(task, :brutal_kill)
   end
 end
