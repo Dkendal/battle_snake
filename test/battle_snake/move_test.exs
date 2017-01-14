f99b5a7e255c054419b55b9932027cee45765502
Dylan Kendal <dylan@stembolt.com>
2017-01-14 13:51:49 -0800

    Default responses from clients that timed-out are used.
    The Move module no accepts a timeout for all requests.
    
    When a request times out the default value will be used.

--- test/battle_snake/move_test.exs
+++ test/battle_snake/move_test.exs
@@ -4,14 +4,27 @@ defmodule BattleSnake.MoveTest  do
   alias BattleSnake.{Snake, Move}
 
   describe "BattleSnake.Move.all/1" do
-    test "returns a move for each snake" do
+    test "returns a default move when the request times-out" do
       move_fn = fn _snake ->
-        %Move{move: "up"}
+        Process.sleep 100
+        %Move{move: "left"}
       end
 
       snakes = [%Snake{}]
 
-      assert match?([%Move{}], Move.all(snakes, move_fn))
+      assert(match?([%Move{move: "up"}],
+            Move.all(snakes, move_fn, 0)))
+    end
+
+    test "returns a move for each snake" do
+      move_fn = fn _snake ->
+        %Move{move: "left"}
+      end
+
+      snakes = [%Snake{}]
+
+      assert(match?([%Move{move: "left"}],
+            Move.all(snakes, move_fn)))
     end
   end
 end
