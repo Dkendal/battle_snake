c0a2f9f715da5de4296460edda6adfd307dd3c83
Dylan Kendal <dylan@stembolt.com>
2017-01-16 15:01:16 -0800

    Add Move.response_state attribute
    the response_state attribute contains an atom which indicates what the state
    of the requrest was, :error, :timeout, or :ok

--- test/battle_snake/move_test.exs
+++ test/battle_snake/move_test.exs
@@ -1,6 +1,5 @@
 defmodule BattleSnake.MoveTest  do
   use ExUnit.Case, async: true
-  use Property
   alias BattleSnake.{Snake, Move, World}
 
   @green_snake %Snake{name: :green}
@@ -10,30 +9,30 @@ defmodule BattleSnake.MoveTest  do
   }
 
   @up %Move{move: "up", snake: @green_snake}
+
   @left %Move{move: "left", snake: @green_snake}
 
-  def up_fn(_snake, _world) do
+  def up_fn(%Snake{}, %World{}) do
     %Move{move: "left"}
   end
 
-  def sleep_fn(_snake, _world) do
+  def sleep_fn(%Snake{}, %World{}) do
     Process.sleep 100
     @left
   end
 
   describe "BattleSnake.Move.all/1" do
     test "returns a default move when the request times-out" do
+      expected = put_in @up.response_state, :timeout
 
-      snakes = [@green_snake]
-
-      assert(match?([@up],
+      assert(match?([^expected],
             Move.all(@world, &sleep_fn/2, 0)))
     end
 
     test "returns a move for each snake" do
-      snakes = [@green_snake]
+      expected = put_in @left.response_state, :ok
 
-      assert(match?([@left],
+      assert(match?([^expected],
             Move.all(@world, &up_fn/2)))
     end
   end
