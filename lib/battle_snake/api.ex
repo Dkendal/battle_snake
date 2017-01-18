82413bf3fdf4b3bc5a229646e2cb92c422678d93
Dylan Kendal <dylan@stembolt.com>
2017-01-18 12:02:57 -0800

    Move Api.Response into it's own file.

--- lib/battle_snake/api.ex
+++ lib/battle_snake/api.ex
@@ -1,62 +1,15 @@
 defmodule BattleSnake.Api do
   alias BattleSnake.{
-    HTTP,
-    SnakeForm,
+    Api.Response,
     GameForm,
-    Snake,
+    HTTP,
     Move,
+    Snake,
+    SnakeForm,
     World}
 
   @callback load(%SnakeForm{}, %GameForm{}) :: Response.t
   @callback move(%Snake{}, %World{}) :: Response.t
-
-  defmodule Response do
-    alias __MODULE__
-
-    @type error :: {:error, any} | {:error, :no_response}
-    @type raw_response :: {:ok, HTTPoison.Response.t} | error
-    @type parsed_response :: {:ok, map | struct} | error
-    @type t :: %__MODULE__{
-      raw_response: raw_response,
-      parsed_response: parsed_response
-    }
-
-    @no_response {:error, :no_response}
-
-    @moduledoc """
-    An api response. Contains the raw response and parsed details.
-    """
-
-    defstruct [
-      raw_response: {:error, :init},
-      parsed_response: {:error, :init}
-    ]
-
-    @spec parse(t, as: struct) :: parsed_response
-    def parse(response, as: as) do
-      parsed_response =
-        with({:ok, %HTTPoison.Response{} = raw} <- response.raw_response,
-             body = raw.body,
-             parsed_result = Poison.decode(body, as: as)) do
-          parsed_result
-        else
-          {:error, _} ->
-            @no_response
-        end
-
-      put_in(response.parsed_response, parsed_response)
-    end
-
-    @spec new(raw_response, as: struct) :: t
-    def new(raw_response, as: as) do
-      parse(%__MODULE__{raw_response: raw_response}, as: as)
-    end
-
-    @spec val(t) :: parsed_response
-    def val(response) do
-      response.parsed_response
-    end
-  end
 
   @doc """
   Load the Snake struct based on the configuration_form data for both the world
