8e249acb6e58e8c168356e549bc0ceb188cec325
Dylan Kendal <dylan@stembolt.com>
2017-01-18 20:23:42 -0800

    Cleanup Poison World test

--- test/battle_snake/world_test.exs
+++ test/battle_snake/world_test.exs
@@ -1,6 +1,5 @@
 defmodule BattleSnake.WorldTest do
   alias BattleSnake.{
-    Board,
     Point,
     Snake,
     World,
@@ -89,58 +88,58 @@ defmodule BattleSnake.WorldTest do
   end
 
   describe "Poison.Encoder.encode(%BattleSnake.World{}, [])" do
-    test "formats as JSON" do
-      head = %{
-        "state" => "head",
-        "snake" => "bar",
-      }
+    use Point
 
-      body = %{
-        "state" => "body",
-        "snake" => "bar",
-      }
-
-      #   0 1
-      # 0   h
-      # 1 f b
-      world = %World{
-        turn: 0,
-        food: [
-          %Point{x: 0, y: 1},
-        ],
-        snakes: [
-          %Snake{
-            coords: [
-              %Point{x: 1, y: 0},
-              %Point{x: 1, y: 1},
-            ],
-            name: "bar",
-            url: "example.com",
-          }
-        ],
-        height: 2,
-        width: 2,
-      }
-
-      actual = PoisonTesting.cast! world
-
-      expected = %{
-        "board" => [
-        [Board.empty, Board.food],
-        [head, body],
+    @world %World{
+      turn: 0,
+      height: 2,
+      width: 2,
+      food: [
+        p(0, 1)
       ],
-        "food" => [[0,1]],
-        "snakes" => [
-          %{
-            "name" => "bar",
-            "coords" => [[1,0],[1,1]],
-            "url" => "example.com",
-          }
-        ],
-        "turn" => 0,
-      }
+      snakes: [
+        %Snake{
+          name: "bar",
+          url: "example.com",
+          coords: [
+            p(1, 0),
+            p(1, 1)
+          ]
+        }
+      ]
+    }
 
-      assert expected == actual
+    @json %{
+      "turn" => 0,
+      "food" => [
+        [0, 1]
+      ],
+      "board" => [
+        [
+          %{"state" => "empty"},
+          %{"state" => "food"}
+        ],
+        [
+          %{"state" => "head", "snake" => "bar"},
+          %{"state" => "body", "snake" => "bar"}
+        ]
+      ],
+      "snakes" => [
+        %{
+          "name" => "bar",
+          "url" => "example.com",
+          "coords" => [
+            [1,0],
+            [1,1]
+          ]
+        }
+      ]
+    }
+
+    @expected PoisonTesting.cast! @world
+
+    test "formats as JSON" do
+      assert @expected == @json
     end
   end
 end
