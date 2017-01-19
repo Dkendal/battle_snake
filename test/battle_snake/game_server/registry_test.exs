33bde24114e3fb0563b68ca46aa2e5b949b20726
Dylan Kendal <dylan@stembolt.com>
2017-01-19 15:59:28 -0800

    Add BattleSnake.GameServer.Registry
    Process Registry for game servers.
    
    Allows Game servers to be registered with its naming mechanisim so that they
    can be accessed by key later.

---
+++ test/battle_snake/game_server/registry_test.exs
@@ -0,0 +1,47 @@
+defmodule BattleSnake.GameServer.RegistryTest do
+  alias BattleSnake.GameServer
+  use ExUnit.Case, async: false
+
+  @state %GameServer.State{}
+  @game_server_name "test-game-server"
+
+  describe "BattleSnake.GameServer.Registry.create/2" do
+    test "registers the process" do
+      assert {:ok, game_server} = GameServer.Registry.create(@game_server_name, @state)
+      assert [{game_server, nil}] == Registry.lookup(GameServer.Registry, @game_server_name)
+      GenServer.stop(game_server)
+    end
+  end
+
+  describe "BattleSnake.GameServer.Registry.lookup/1" do
+    setup [:start_game_server]
+
+    test "fetches the process by the name it was registered under", %{game_server: game_server} do
+      assert [{game_server, nil}] == GameServer.Registry.lookup(@game_server_name)
+      GenServer.stop(game_server)
+    end
+  end
+
+  describe "BattleSnake.GameServer.Registry.lookup_or_create/2" do
+    test "starts the process if it isn't already registered" do
+      assert {:ok, game_server} =
+        GameServer.Registry.lookup_or_create(@game_server_name, @state)
+
+      GenServer.stop(game_server)
+    end
+
+    test "returns the registered process if it already exists" do
+      %{game_server: game_server} = start_game_server()
+
+      assert {:ok, ^game_server} =
+        GameServer.Registry.lookup_or_create(@game_server_name, @state)
+
+      GenServer.stop(game_server)
+    end
+  end
+
+  def start_game_server(context \\ %{}) do
+    {:ok, game_server} = GameServer.Registry.create(@game_server_name, @state)
+    Map.put(context, :game_server, game_server)
+  end
+end
