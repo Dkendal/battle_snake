4bb536a5ba5af8ed7adc4100a1c061aa343b6e4c
Dylan Kendal <dylan@stembolt.com>
2017-01-20 14:06:48 -0800

    Add GameServer.get_state/1
    allows the state to be inspected.

--- test/battle_snake/game_server_test.exs
+++ test/battle_snake/game_server_test.exs
@@ -127,4 +127,11 @@ defmodule BattleSnake.GameServerTest do
       assert_receive {:ping, ^prev}
     end
   end
+
+  describe "BattleSnake.GameServer.get_state/1" do
+    test "returns the state of the game server" do
+      {:ok, pid} = GameServer.start_link(@state)
+      assert {:suspend, @state} == GameServer.get_state(pid)
+    end
+  end
 end
