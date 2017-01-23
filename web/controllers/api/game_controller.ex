96704be0285cbbbe5b85d495b86523a465b6d2b8
Dylan Kendal <dylan@stembolt.com>
2017-01-23 09:31:32 -0800

    api_game_controller serves json

--- web/controllers/api/game_controller.ex
+++ web/controllers/api/game_controller.ex
@@ -1,6 +1,12 @@
 defmodule BattleSnake.Api.GameController do
+  alias BattleSnake.{
+    GameForm
+  }
+
   use BattleSnake.Web, :controller
 
   def index(conn, __params) do
+    games = GameForm.all
+    render(conn, "index.json", games: games)
   end
 end
