5d7dbb474bddc4e8a5b44320110bbee4ad21e8ef
Dylan Kendal <dylan@stembolt.com>
2017-01-23 09:19:06 -0800

    Remove comment.

--- web/controllers/game_controller.ex
+++ web/controllers/game_controller.ex
@@ -4,7 +4,7 @@ defmodule BattleSnake.GameController do
   alias BattleSnake.GameForm
 
   def index(conn, _params) do
-    games = GameForm.all  # => nil
+    games = GameForm.all
 
     game_servers = for game <- games do
       status = with [{pid, _}] <- BattleSnake.GameServer.Registry.lookup(game.id) do
