51d6ea2ee563e6c302f25c7b03bfd2c48c990eef
Dylan Kendal <dylan@stembolt.com>
2017-01-24 14:57:34 -0800

    Cleanup Repo, fix issue with save not checking type
    Also made it so changesets will be applied before saving.

--- lib/mnesia/repo.ex
+++ lib/mnesia/repo.ex
@@ -11,8 +11,35 @@ defmodule Mnesia.Repo do
   conjunction with ETS.
   """
 
+  @doc """
+  Precompilation callback. save/1 needs to be appended to module defintion
+  because __struct__ must be defined first.
+  """
+  defmacro __before_compile__(_env) do
+    quote do
+      def save(%Ecto.Changeset{} = changeset) do
+        changeset
+        |> Ecto.Changeset.apply_changes()
+        |> save()
+      end
+
+      def save(%__MODULE__{} = struct) do
+        write = fn ->
+          :mnesia.write(record(struct))
+        end
+
+        {:atomic, :ok} = :mnesia.transaction(write)
+
+        struct
+      end
+    end
+  end
+
   defmacro __using__(_) do
     quote do
+      @before_compile unquote __MODULE__
+      @table_name __MODULE__
+
       use Ecto.Schema
 
       def table, do: [attributes: fields()]
@@ -22,57 +49,47 @@ defmodule Mnesia.Repo do
       def record(struct) do
         get = &Map.get(struct, &1)
         attrs = Enum.map(fields(), get)
-        List.to_tuple [__MODULE__ |attrs]
+        List.to_tuple [@table_name |attrs]
       end
 
       def load(record) do
-        [__MODULE__ |attrs] = Tuple.to_list(record)
+        [@table_name |attrs] = Tuple.to_list(record)
         attrs = Enum.zip(fields(), attrs)
-        struct(__MODULE__, attrs)
+        struct(@table_name, attrs)
       end
 
       def all do
         fn ->
-          :qlc.e(:mnesia.table __MODULE__)
+          :qlc.e(:mnesia.table @table_name)
         end
         |> :mnesia.async_dirty()
         |> Enum.map(&load/1)
       end
 
       def last do
-        load(:mnesia.last(__MODULE__))
-      end
-
-      def save(struct) do
-        write = fn ->
-          :mnesia.write(record(struct))
-        end
-
-        {:atomic, :ok} = :mnesia.transaction(write)
-
-        struct
+        load(:mnesia.last(@table_name))
       end
 
       def get(id) do
         read = fn ->
-          :mnesia.read(__MODULE__, id)
+          :mnesia.read(@table_name, id)
         end
 
         with {:atomic, [record]} <- :mnesia.transaction(read) do
           {:ok, load(record)}
         else
           {:atomic, []} ->
-            {:error, %Mnesia.RecordNotFoundError{id: id, table: __MODULE__}}
+            {:error, %Mnesia.RecordNotFoundError{id: id, table: @table_name}}
         end
       end
 
       @spec create_table(Keyword.t) :: {:atomic, :ok} | {:aborted, any}
       def create_table(opts \\ []) do
-        :mnesia.create_table(__MODULE__, opts ++ table())
+        :mnesia.create_table(@table_name, opts ++ table())
       end
 
       def delete_table do
-        :mnesia.delete_table(__MODULE__)
+        :mnesia.delete_table(@table_name)
       end
     end
   end
