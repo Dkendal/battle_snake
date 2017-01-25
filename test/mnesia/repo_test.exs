1eaa035e9cbf2d48c528d5afc759d1d1ec5a6af3
Dylan Kendal <dylan@stembolt.com>
2017-01-25 15:24:30 -0800

    Save mnesia record autogenerates an id
    Just uses a reference for the ID. there is a risk of collision every 2^82 calls
    which is fine in practice.

--- test/mnesia/repo_test.exs
+++ test/mnesia/repo_test.exs
@@ -18,102 +18,103 @@ defmodule Mnesia.RepoTest.DummyModel do
   end
 end
 
-defmodule Mnesia.RepoTest.Macros do
-  defmacro shared_examples(module) do
-    quote do
-      @described_module unquote(module)
-      @model struct(@described_module, id: 0, x: 1, y: 2)
-      @record {@described_module, 0, 1, 2}
-
-      describe "Mnesia.Repo.fields/0" do
-        test "returns the column names" do
-          assert @described_module.fields == [
-            :id,
-            :x,
-            :y]
-        end
-      end
-
-      describe "Mnesia.Repo.table/0" do
-        test "returns the decleration for mnesia" do
-          assert @described_module.table == [
-            attributes: [
-              :id,
-              :x,
-              :y]]
-        end
-      end
-
-      describe "Mnesia.Repo.record/1" do
-        test "converts the struct to a record" do
-          assert @described_module.record(@model) == @record
-        end
-      end
-
-      describe "Mnesia.Repo.load/1" do
-        test "converts the struct to a record" do
-          assert @described_module.load(@record) == @model
-        end
-      end
-
-      describe "Mnesia.Repo.save/1" do
-        setup [:create_table, :delete_table]
-
-        test "writes the record to mnesia" do
-          assert @model == @described_module.save(@model)
-          assert 1 == :mnesia.table_info(@described_module, :size)
-        end
-      end
-
-      describe "Mnesia.Repo.get/1" do
-        setup [:create_table, :delete_table, :create_dummy]
-
-        test "returns the record", %{dummy: dummy} do
-          assert {:ok, @model} == @described_module.get(dummy.id)
-        end
-
-        test "returns an error when the record doesn't exist" do
-          assert {:error, %Mnesia.RecordNotFoundError{table: @described_module, id: "fake-record"}} ==
-            @described_module.get("fake-record")
-        end
-      end
-
-      describe "Mnesia.Repo.create_table/0" do
-        setup [:delete_table]
-
-        test "creates the mnesia table" do
-          assert {:atomic, :ok} == @described_module.create_table()
-          assert @described_module == :mnesia.table_info(unquote(module), :record_name)
-        end
-      end
-
-      def create_dummy(context \\ %{}) do
-        dummy = @described_module.save(@model)
-        Map.put(context, :dummy, dummy)
-      end
-
-      def create_table(context \\ %{}) do
-        {:atomic, :ok} = @described_module.create_table()
-        context
-      end
-
-      def delete_table(context \\ %{}) do
-        on_exit fn ->
-          {:atomic, :ok} = @described_module.delete_table()
-        end
-        context
-      end
-    end
-  end
-end
-
 defmodule Mnesia.RepoTest do
   alias __MODULE__.DummyModel
   alias __MODULE__.DummyStruct
   use ExUnit.Case, async: false
 
-  require __MODULE__.Macros
-  import __MODULE__.Macros
+  @described_module DummyModel
+  @model struct(@described_module, id: 0, x: 1, y: 2)
+  @record {@described_module, 0, 1, 2}
 
-  Mnesia.RepoTest.Macros.shared_examples(DummyModel)
+  describe "Mnesia.Repo.fields/0" do
+    test "returns fields defined as a module attribute" do
+      assert DummyStruct.fields == [
+        :id,
+        :x,
+        :y]
+    end
+
+    test "returns the column names" do
+      assert @described_module.fields == [
+        :id,
+        :x,
+        :y]
+    end
+  end
+
+  describe "Mnesia.Repo.table/0" do
+    test "returns the decleration for mnesia" do
+      assert @described_module.table == [
+        attributes: [
+          :id,
+          :x,
+          :y]]
+    end
+  end
+
+  describe "Mnesia.Repo.record/1" do
+    test "converts the struct to a record" do
+      assert @described_module.record(@model) == @record
+    end
+  end
+
+  describe "Mnesia.Repo.load/1" do
+    test "converts the struct to a record" do
+      assert @described_module.load(@record) == @model
+    end
+  end
+
+  describe "Mnesia.Repo.save/1" do
+    setup [:create_table, :delete_table]
+
+    test "writes the record to mnesia" do
+      assert @model == @described_module.save(@model)
+      assert 1 == :mnesia.table_info(@described_module, :size)
+    end
+
+    test "adds an id if the @primary_key is nil" do
+      id = @described_module.save(%{@model| id: nil}).id
+      assert is_reference id
+    end
+  end
+
+  describe "Mnesia.Repo.get/1" do
+    setup [:create_table, :delete_table, :create_dummy]
+
+    test "returns the record", %{dummy: dummy} do
+      assert {:ok, @model} == @described_module.get(dummy.id)
+    end
+
+    test "returns an error when the record doesn't exist" do
+      assert {:error, %Mnesia.RecordNotFoundError{table: @described_module, id: "fake-record"}} ==
+        @described_module.get("fake-record")
+    end
+  end
+
+  describe "Mnesia.Repo.create_table/0" do
+    setup [:delete_table]
+
+    test "creates the mnesia table" do
+      assert {:atomic, :ok} == @described_module.create_table()
+      assert @described_module == :mnesia.table_info(@described_module, :record_name)
+    end
+  end
+
+  def create_dummy(context \\ %{}) do
+    dummy = @described_module.save(@model)
+    Map.put(context, :dummy, dummy)
+  end
+
+  def create_table(context \\ %{}) do
+    {:atomic, :ok} = @described_module.create_table()
+    context
+  end
+
+  def delete_table(context \\ %{}) do
+    on_exit fn ->
+      {:atomic, :ok} = @described_module.delete_table()
+    end
+    context
+  end
 end
