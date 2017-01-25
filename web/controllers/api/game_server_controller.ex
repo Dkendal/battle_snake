f16cff09c3ffec24048bf26796d451460ad8bd7c
Dylan Kendal <dylan@stembolt.com>
2017-01-24 16:53:11 -0800

    Api.GameServerController Starts up a server on create

--- web/controllers/api/game_server_controller.ex
+++ web/controllers/api/game_server_controller.ex
@@ -1,7 +1,48 @@
 defmodule BattleSnake.Api.GameServerController do
+  alias BattleSnake.{
+    GameForm,
+    GameServer
+  }
+
   use BattleSnake.Web, :controller
 
   def create(conn, %{"id" => id}) do
-    render conn, game_server: %{}
+    try do
+      id
+      |> GameServer.Registry.lookup()
+      |> do_create(id: id)
+
+      json conn, :ok
+    rescue
+      e ->
+        json conn, %{error: e.message}
+    end
+  end
+
+  def do_create([], id: id) do
+    GameForm.get(id)
+    |> do_create(id: id)
+  end
+
+  def do_create([{pid, _}], _) when is_pid(pid) do
+    raise "game server already started"
+  end
+
+  def do_create({:error, e}, _) do
+    raise e
+  end
+
+  def do_create({:ok, %GameForm{} = game_form}, id: id) do
+    state = BattleSnake.GameServerConfig.setup(game_form, on_change())
+    {:ok, game_server_pid} = GameServer.Registry.create(id, state)
+    GameServer.resume(game_server_pid)
+  end
+
+  defp on_change() do
+    fn state ->
+      require Logger
+      Logger.debug "tick | pid: #{inspect self()} | turn: #{state.world.turn}"
+      state
+    end
   end
 end
