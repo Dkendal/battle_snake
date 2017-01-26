94bc798a9ab694521d3d0fc84e4a4a54d6bad515
Dylan Kendal <dylan@stembolt.com>
2017-01-26 11:06:23 -0800

    On ExUnit start setup Mnesia
    instead of running the mix task this just does the same thing, but sets up
    things only in memory.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,10 @@
+:stopped = :mnesia.stop
+:ok = :mnesia.delete_schema([node()])
+:ok = :mnesia.create_schema([node()])
+:ok = :mnesia.start()
+
+{:atomic, :ok} = BattleSnake.GameForm.create_table(ram_copies: [node()])
+{:atomic, :ok} = BattleSnake.World.create_table(ram_copies: [node()])
+
 ExUnit.start
 ExUnit.plural_rule("property", "properties")
-Mix.Tasks.BattleSnake.Createdb.run []
