cd0d250527193ad9eefe226f997ef896c192b897
Dylan Kendal <dylankendal@gmail.com>
2017-01-26 11:09:16 -0800

    Don't blow alawy mnesia schema on start up
    Default mnesia behaviour is not require a disc schema if tables all reside in
    memory. Because all our test tables are in memory they shouldn't exist on start
    up.
    
    If creating a table fails it means it already exists and we would be clobbering
    it.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,8 +1,3 @@
-:stopped = :mnesia.stop
-:ok = :mnesia.delete_schema([node()])
-:ok = :mnesia.create_schema([node()])
-:ok = :mnesia.start()
-
 {:atomic, :ok} = BattleSnake.GameForm.create_table(ram_copies: [node()])
 {:atomic, :ok} = BattleSnake.World.create_table(ram_copies: [node()])
 
