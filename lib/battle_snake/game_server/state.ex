4916eb3f5efce36cb8b993c4b9c1f814c23679f1
Dylan Kendal <dylan@stembolt.com>
2017-01-27 10:21:21 -0800

    Move everything that isn't a client/server api out of game server
    All of the functions that were state manipulations now live in GameServer.State,
    where they belong.

--- lib/battle_snake/game_server/state.ex
+++ lib/battle_snake/game_server/state.ex
@@ -1,10 +1,12 @@
 defmodule BattleSnake.GameServer.State do
   alias __MODULE__
 
+  @max_history 20
+
   @type t :: %State{
     world: BattleSnake.World.t,
     reducer: (t -> t),
-    on_change: (t-> t),
+  on_change: (t-> t),
     opts: [any],
     hist: [t],
     game_form: BattleSnake.GameForm.t
@@ -47,4 +49,40 @@ defmodule BattleSnake.GameServer.State do
   end
 
   def identity(x), do: x
+
+  def step(state) do
+    state
+    |> save_history()
+    |> apply_reducer()
+    |> on_change()
+  end
+
+  defp apply_reducer(%{world: w, reducer: f} = state) do
+    %{state| world: f.(w)}
+  end
+
+  defp save_history(%{world: h} = state) do
+    update_in state.hist, fn t ->
+      [h |Enum.take(t, @max_history)]
+    end
+  end
+
+  def step_back(%{hist: []} = s), do: s
+
+  def step_back(state) do
+    state
+    |> prev_turn
+    |> State.on_change()
+  end
+
+  def delay(state) do
+    opts = state.opts
+    Keyword.fetch!(opts, :delay)
+  end
+
+  defp prev_turn(state) do
+    [h|t] = state.hist
+    state = put_in state.world, h
+    put_in(state.hist, t)
+  end
 end
--- test/battle_snake/game_server/state_test.exs
+++ test/battle_snake/game_server/state_test.exs
@@ -1,2 +1,31 @@
 defmodule BattleSnake.GameServer.StateTest do
+  alias BattleSnake.GameServer.State
+
+  use BattleSnake.Case, async: true
+
+  @opts [delay: 0]
+  @state %State{world: 10, hist: [9, 8, 7], opts: @opts}
+  @prev %State{world: 9, hist: [8, 7], opts: @opts}
+  @empty_state %State{world: 1, hist: []}
+
+  def ping(pid), do: &send(pid, {:ping, &1})
+
+  describe "State.step_back/1" do
+    test "does nothing when the history is empty" do
+      assert State.step_back(@empty_state) == @empty_state
+    end
+
+    test "rewinds the state to the last move" do
+      assert State.step_back(@state) == @prev
+    end
+
+    test "calls the on_change function" do
+      state = %{@state| on_change: ping(self())}
+      prev = %{@prev| on_change: ping(self())}
+
+      State.step_back(state)
+
+      assert_receive {:ping, ^prev}
+    end
+  end
 end
