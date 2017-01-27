da52c1070ea9dff0baf5140e53b685c1dbdf3556
Dylan Kendal <dylankendal@gmail.com>
2017-01-26 17:00:46 -0800

    Refactor GameServer
    Reorganized function definitions in GameServer with the intention of moving
    state of {status, GameServer.State} into GameServer.State.state.
    
    To this end all server_callbacks are grouped under their request atom and the
    status matching is done in a case statement in the function definition.
    
    This does also make reading this file significantly easier.

--- lib/battle_snake/game_server.ex
+++ lib/battle_snake/game_server.ex
@@ -6,6 +6,8 @@ defmodule BattleSnake.GameServer do
 
   @type state :: State.t
 
+  # Begin Client Api
+
   def start_link(%State{} = state, opts \\ []) do
     state = State.on_start(state)
     GenServer.start_link(__MODULE__, {:suspend, state}, opts)
@@ -19,102 +21,18 @@ defmodule BattleSnake.GameServer do
     GenServer.call(pid, :get_status)
   end
 
-  def handle_call(:get_status, _from, {status, _} = state) do
-    {:reply, status, state}
-  end
-
-  def handle_call(:next, _from, {:halted, state}) do
-    {:reply, :ok, {:halted, state}}
-  end
-
-  def handle_call(:next, _from, {_, state}) do
-    state = step(state)
-    {:reply, :ok, {:suspend, state}}
-  end
-
-  def handle_call(:on_change, _from, {status, state}) do
-    state = State.on_change(state)
-    {:reply, :ok, {status, state}}
-  end
-
-  def handle_call(:on_done, _from, {status, state}) do
-    state = State.on_done(state)
-    {:reply, :ok, {status, state}}
-  end
-
-  def handle_call(:on_start, _from, {status, state}) do
-    state = State.on_start(state)
-    {:reply, :ok, {status, state}}
-  end
-
-  def handle_call(:pause, _from, {:cont, state}) do
-    {:reply, :ok, {:suspend, state}}
-  end
-
-  def handle_call(:pause, _from, state) do
-    {:reply, :ok, state}
-  end
-
-  def handle_call(:prev, _from, {:halted, state}) do
-    {:reply, :ok, {:halted, state}}
-  end
-
-  def handle_call(:prev, _from, {_, state}) do
-    state = state
-    |> step_back()
-    {:reply, :ok, {:suspend, state}}
-  end
-
-  def handle_call(:resume, _from, {:suspend, state}) do
-    tick(state)
-    {:reply, :ok, {:cont, state}}
-  end
-
-  def handle_call(:resume, _from, state) do
-    {:reply, :ok, state}
-  end
-
-  def handle_call(request, from, state) do
-    # Call the default implementation from GenServer
-    super(request, from, state)
-  end
-
-  def handle_cast(request, state) do
-    super(request, state)
-  end
-
-  def handle_info(:get_state, {_, state} = s) do
-    {:reply, state, s}
-  end
-
-  def handle_info(:tick, {:cont, state}) do
-    state = step(state)
-    if State.done?(state) do
-      state = State.on_done(state)
-      {:noreply, {:halted, state}}
-    else
-      tick(state)
-      {:noreply, {:cont, state}}
-    end
-  end
-
-  def handle_info(:tick, {:halted, state}) do
-    {:noreply, {:halted, state}}
-  end
-
-  def handle_info(:tick, {:suspend, state}) do
-    {:noreply, {:suspend, state}}
-  end
-
   def next(pid) do
     GenServer.call(pid, :next)
   end
 
-  def on_change(pid), do: GenServer.call(pid, :on_change)
+  def on_change(pid),
+    do: GenServer.call(pid, :on_change)
 
-  def on_done(pid), do: GenServer.call(pid, :on_done)
+  def on_done(pid),
+    do: GenServer.call(pid, :on_done)
 
-  def on_start(pid), do: GenServer.call(pid, :on_start)
+  def on_start(pid),
+    do: GenServer.call(pid, :on_start)
 
   def pause(pid) do
     GenServer.call(pid, :pause)
@@ -167,4 +85,110 @@ defmodule BattleSnake.GameServer do
   defp tick(state) do
     Process.send_after(self(), :tick, delay(state))
   end
+
+  # End Client Api
+
+  # Begin Server Callbacks
+
+  def handle_call(:get_status, _from, {status, _} = state) do
+    {:reply, status, state}
+  end
+
+  def handle_call(:next, _from, {status, state}) do
+    case status do
+      :halted ->
+        {:reply, :ok, {:halted, state}}
+
+      _ ->
+        state = step(state)
+        {:reply, :ok, {:suspend, state}}
+    end
+  end
+
+  def handle_call(:on_change, _from, {status, state}) do
+    state = State.on_change(state)
+    {:reply, :ok, {status, state}}
+  end
+
+  def handle_call(:on_done, _from, {status, state}) do
+    state = State.on_done(state)
+    {:reply, :ok, {status, state}}
+  end
+
+  def handle_call(:on_start, _from, {status, state}) do
+    state = State.on_start(state)
+    {:reply, :ok, {status, state}}
+  end
+
+  def handle_call(:pause, _from, state) do
+    case state do
+      {:cont, state} ->
+        {:reply, :ok, {:suspend, state}}
+
+      state ->
+        {:reply, :ok, state}
+    end
+  end
+
+  def handle_call(:prev, _from, state) do
+    case state do
+      {:halted, state} ->
+        {:reply, :ok, {:halted, state}}
+
+      {_, state} ->
+        state = state
+        |> step_back()
+        {:reply, :ok, {:suspend, state}}
+    end
+  end
+
+  def handle_call(:resume, _from, state) do
+    case state do
+      {:suspend, state} ->
+        tick(state)
+        {:reply, :ok, {:cont, state}}
+
+      state ->
+        {:reply, :ok, state}
+    end
+  end
+
+  def handle_call(request, from, state) do
+    # Call the default implementation from GenServer
+    super(request, from, state)
+  end
+
+  def handle_cast(request, state) do
+    super(request, state)
+  end
+
+  def handle_info(:get_state, {_, state} = s) do
+    {:reply, state, s}
+  end
+
+  def handle_info(:tick, state) do
+    case state do
+      {:cont, state} ->
+        state = step(state)
+        if State.done?(state) do
+          state = State.on_done(state)
+          {:noreply, {:halted, state}}
+        else
+          tick(state)
+          {:noreply, {:cont, state}}
+        end
+
+      {:halted, state} ->
+        {:noreply, {:halted, state}}
+
+      {:suspend, state} ->
+        {:noreply, {:suspend, state}}
+    end
+  end
+
+  def handle_info(request, state) do
+    super(request, state)
+  end
+
+  # End Server Callbacks
 end
