d027b8875216fcddfdced977d5f5df94dba6fcee
Dylan Kendal <dylan@stembolt.com>
2017-01-31 09:06:47 -0800

    Use aliases in test

--- test/battle_snake/game_server/supervisor_test.exs
+++ test/battle_snake/game_server/supervisor_test.exs
@@ -1,19 +1,23 @@
 defmodule BattleSnake.GameServer.SupervisorTest do
+  alias BattleSnake.{
+    GameServer,
+  }
+
   use BattleSnake.Case, async: false
 
-  @sup_name BattleSnake.GameServer.Supervisor
+  @sup_name GameServer.Supervisor
 
-  describe "BattleSnake.GameServer.Supervisor.start_game_server/1" do
-    import BattleSnake.GameServer.Supervisor, only: [start_game_server: 1]
+  describe "GameServer.Supervisor.start_game_server/1" do
+    import GameServer.Supervisor, only: [start_game_server: 1]
 
     test "starts a game server process" do
-      {:ok, game_server} = start_game_server([%BattleSnake.GameServer.State{}])
+      {:ok, game_server} = start_game_server([%GameServer.State{}])
       assert is_pid game_server
       assert %{active: 1, workers: 1} = Supervisor.count_children(@sup_name)
     end
 
     test "does not restart processes that have died" do
-      {:ok, game_server} = start_game_server([%BattleSnake.GameServer.State{}])
+      {:ok, game_server} = start_game_server([%GameServer.State{}])
       GenServer.stop(game_server, :normal)
       ref = Process.monitor(game_server)
       assert_receive {:DOWN, ^ref, _, _, _}
