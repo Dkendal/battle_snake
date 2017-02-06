686816b76959ef11cc5bdf2233a5e6ad27387aa1
Dylan Kendal <dylankendal@gmail.com>
2017-02-06 15:26:17 -0800

    Added ability for admin channel to push commands

--- test/channels/game_admin_channel_test.exs
+++ test/channels/game_admin_channel_test.exs
@@ -2,14 +2,31 @@ defmodule BattleSnake.GameAdminChannelTest do
   use BattleSnake.ChannelCase
 
   alias BattleSnake.GameAdminChannel
+  alias BattleSnake.GameServer
 
   setup do
-    game_id = 1
+    game_id = "1"
 
     {:ok, _, socket} =
       socket("user_id", %{some: :assign})
       |> subscribe_and_join(GameAdminChannel, "game_admin:" <> game_id)
 
     {:ok, socket: socket}
+  end
+
+  describe "push $command" do
+    setup c do
+      ref = push(c.socket, "resume")
+      [ref: ref]
+    end
+
+    test "responds with ok", c do
+      assert_reply c.ref, :ok
+    end
+
+    test "sends message to the pub sub", c do
+      GameServer.PubSub.subscribe("1")
+      assert_receive {:command, :resume}
+    end
   end
 end
--- web/channels/game_admin_channel.ex
+++ web/channels/game_admin_channel.ex
@@ -1,16 +1,35 @@
 defmodule BattleSnake.GameAdminChannel do
+  alias BattleSnake.GameServer
   use BattleSnake.Web, :channel
 
   def join("game_admin:" <> game_id, payload, socket) do
     if authorized?(payload) do
+      socket = assign(socket, :game_id, game_id)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
+  @command_list ~w(resume next prev)
+  def handle_in(command, _, socket) when command in @command_list do
+    cmd = String.to_existing_atom(command)
+    issue_command(socket, cmd)
+    {:reply, :ok, socket}
+  end
+
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  defp game_id(socket) do
+    socket.assigns.game_id
+  end
+
+  defp issue_command(socket, name) do
+    socket
+    |> game_id()
+    |> GameServer.PubSub.broadcast!({:command, name})
   end
 end
