76e75944ede8666d02ceca61f4b218ab52aa8af5
Dylan Kendal <dylankendal@gmail.com>
2017-02-06 14:18:11 -0800

    Added the ability for board viewer to render different content types

--- web/channels/board_viewer_channel.ex
+++ web/channels/board_viewer_channel.ex
@@ -3,9 +3,16 @@ defmodule BattleSnake.BoardViewerChannel do
 
   use BattleSnake.Web, :channel
 
+  @typedoc """
+  Optional values:
+      "contentType" => "html" | "json"
+  """
+  @type join_payload :: %{optional(binary) => binary}
+  @spec join(binary, join_payload, Phoenix.Socket.t) :: {:ok, Phoenix.Socket} | {:error, any}
   def join("board_viewer:" <> game_id, payload, socket) do
     if authorized?(payload) do
       GameServer.PubSub.subscribe(game_id)
+      socket = set_content_type(socket, payload)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -13,13 +20,31 @@ defmodule BattleSnake.BoardViewerChannel do
   end
 
   def handle_info(%GameServer.State.Event{name: name, data: state}, socket) do
-    html = Phoenix.View.render_to_string(BattleSnake.PlayView, "board.html", state: state)
-    broadcast(socket, "tick", %{content: html})
+    content = render_content(socket.assigns.content_type, state)
+    broadcast(socket, "tick", %{content: content})
     {:noreply, socket}
   end
 
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  defp render_content(:json, state) do
+    Poison.encode!(state.world)
+  end
+
+  defp render_content(_, state) do
+    Phoenix.View.render_to_string(BattleSnake.PlayView, "board.html", state: state)
+  end
+
+  defp set_content_type(socket, payload) do
+    content_type =
+      case payload["contentType"] do
+        "html" -> :html
+        "json" -> :json
+        _ -> :html
+      end
+    assign socket, :content_type, content_type
   end
 end
