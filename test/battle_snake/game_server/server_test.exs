9138e01c2236ff8edb6ff8e924b09dab234ab0a1
Dylan Kendal <dylan@stembolt.com>
2017-02-07 14:37:45 -0800

    Server can initialize itself
    Allow the GameServer to initialize itself with just an id or with a game form
    struct.

--- test/battle_snake/game_server/server_test.exs
+++ test/battle_snake/game_server/server_test.exs
@@ -1,4 +1,36 @@
 defmodule BattleSnake.GameServer.ServerTest do
   alias BattleSnake.GameServer.Server
+  alias BattleSnake.GameServer.State
+  alias BattleSnake.GameForm
   use BattleSnake.Case, async: true
+
+  def create_game_form(_) do
+    [game_form: create(:game_form)]
+  end
+
+  describe "Server.init(integer)" do
+    setup [:create_game_form]
+
+    test "initializes the game state with the id", c do
+      assert {:ok, state} = Server.init(c.game_form.id)
+      assert state.game_form_id == c.game_form.id
+      assert state.game_form.id == c.game_form.id
+    end
+  end
+
+  describe "Server.init(GameForm.t)" do
+    setup [:create_game_form]
+
+    test "initializes the game state with the id", c do
+      assert {:ok, state} = Server.init(c.game_form)
+      assert state.game_form_id == c.game_form.id
+      assert state.game_form.id == c.game_form.id
+    end
+  end
+
+  describe "Server.init(State.t)" do
+    test "returns ok" do
+      assert {:ok, %State{}} == Server.init(%State{})
+    end
+  end
 end
