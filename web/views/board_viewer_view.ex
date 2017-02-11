681eee695813a5396ccb706bf2326d83aca5deb7
Dylan Kendal <dylan@stembolt.com>
2017-02-10 21:10:12 -0800

    Added board dividers.

--- web/views/board_viewer_view.ex
+++ web/views/board_viewer_view.ex
@@ -1,7 +1,7 @@
 defmodule BattleSnake.BoardViewerView do
   use BattleSnake.Web, :view
 
-  @snake_stroke_width 0.1
+  @snake_stroke_width 0.03
   @food_r 0.25
 
   def snake_obj(point, snake, index) do
@@ -10,8 +10,8 @@ defmodule BattleSnake.BoardViewerView do
     content_tag(:rect, "",
       y: (point.y + @snake_stroke_width),
       x: (point.x + @snake_stroke_width),
-      width: (1 - @snake_stroke_width),
-      height: (1 - @snake_stroke_width),
+      width: (1 - @snake_stroke_width * 2),
+      height: (1 - @snake_stroke_width * 2),
       fill: snake.color,
       fill_opacity: fill_opacity,
       class: "obj-snake")
