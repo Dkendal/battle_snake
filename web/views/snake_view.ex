72cf02f7889009e059dfd7f7c8ce364f7541cfd8
Dylan Kendal <dylankendal@gmail.com>
2017-02-24 22:50:43 -0800

    Add cause of death to scoreboard

--- web/views/snake_view.ex
+++ web/views/snake_view.ex
@@ -1,4 +1,5 @@
 defmodule BattleSnake.SnakeView do
+  alias BattleSnake.Death
   use BattleSnake.Web, :view
 
   def head_image_url(%{url: base, head_url: "/" <> url}) do
@@ -7,5 +8,21 @@ defmodule BattleSnake.SnakeView do
 
   def head_image_url(%{head_url: url}) do
     url
+  end
+
+  def cause_of_death_text(cause) do
+    alias BattleSnake.Death
+    case cause do
+      %Death.StarvationCause{} ->
+        "Starved to death"
+      %Death.WallCollisionCause{} ->
+        "Crashed into a wall"
+      %Death.BodyCollisionCause{with: id} ->
+        "Collided with #{id}'s body"
+      %Death.HeadCollisionCause{with: id} ->
+        "Consumed by #{id}"
+      _ ->
+        ""
+    end
   end
 end
