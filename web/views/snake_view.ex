5f003abf5c3db42278ae781d9046e423031012d0
Dylan Kendal <dylankendal@gmail.com>
2017-02-25 20:09:20 -0800

    Draw snake segments as quadradic curves

--- web/views/snake_view.ex
+++ web/views/snake_view.ex
@@ -1,6 +1,11 @@
 defmodule BattleSnake.SnakeView do
+  alias BattleSnake.Point
   alias BattleSnake.Death
+  alias BattleSnake.Snake
   use BattleSnake.Web, :view
+  use BattleSnake.Point
+
+  @polyline_term_offset 0.38
 
   def head_image_url(%{url: base, head_url: "/" <> url}) do
     base <> "/" <> url
@@ -23,6 +28,113 @@ defmodule BattleSnake.SnakeView do
         "Consumed by #{id}"
       _ ->
         ""
+    end
+  end
+
+  def snake_path(snake) do
+    Enum.reduce(snake.coords, [{nil, nil}], fn v, acc ->
+      case {v, acc} do
+        {v, [{nil, nil} | acc]} ->
+          [{v, nil} | acc]
+
+        {v, [{s, nil} | acc]} ->
+          [{s, v} | acc]
+
+        {p(x, _y0), [{p(x, _y1) = s, p(x, _y2)} | acc]} ->
+          [{s, v} | acc]
+
+        {p(_x0, y), [{p(_x1, y) = s, p(_x2, y)} | acc]} ->
+          [{s, v} | acc]
+
+        {v, [{_, e} | _] = acc} ->
+          [{e, v} | acc]
+
+        {v, acc} ->
+          [{v, nil} | acc]
+      end
+    end)
+    |> do_snake_path
+  end
+
+  defp scale_vector({a, b}) do
+    import Point
+    offset = 0.39
+
+    c = b
+    |> sub(a)
+    |> mul(0.5)
+    |> add(b)
+
+    a0 = a
+    |> sub(c)
+    sa = if mag(a0) == 0,
+      do: 0,
+      else: (mag(a0) - offset) / mag(a0)
+    a0 = a0
+    |> mul(sa)
+    |> add(c)
+
+    b0 = b
+    |> sub(c)
+    sb = if mag(b0) == 0,
+      do: 0,
+      else: (mag(b0) + offset) / mag(b0)
+    b0 = b0
+    |> mul(sb)
+    |> add(c)
+
+    {a0, b0}
+  end
+
+  defp snake_curve(p(ax, ay), p(bx, by)) do
+    [ay, ax, by, bx, "Q"]
+  end
+
+  defp do_snake_path(_, _ \\ [])
+
+  defp do_snake_path([], acc) do
+    a = acc
+    |> Stream.map(& to_string &1)
+    |> Stream.map(& [&1, " "])
+    |> Enum.reverse
+  end
+
+  defp do_snake_path([{a, b} | rest], []) do
+    {a0, b0} = scale_vector({a, b})
+    acc = [a0.y, a0.x, "L", b0.y, b0.x, "M"]
+    do_snake_path(rest, acc)
+  end
+
+  defp do_snake_path([{a, b} | rest], acc) do
+    {a0, b0} = scale_vector({a, b})
+    q =  snake_curve(b0, b)
+    acc = q ++ acc
+    acc = [a0.y, a0.x, "L", b0.y, b0.x, "L" | acc]
+    do_snake_path(rest, acc)
+  end
+
+  def dead_snakes(world) do
+    Enum.sort_by(world.dead_snakes, &(&1.name), &<=/2)
+  end
+
+  def snakes(world) do
+    Enum.sort_by(world.snakes, &(&1.name), &<=/2)
+  end
+
+  def transform_segment(_, []) do
+    ""
+  end
+
+  def transform_segment(h1, [h2]) do
+    v = Point.sub(h2, h1)
+    sx = -1
+    sy = 1
+    p(cx, cy) =  Point.add h1, 0.5
+    case v do
+      p(1, 0) -> "matrix(#{sx}, 0, 0, #{sy}, #{cx-sx*cx}, #{cy-sy*cy})" # flip horizontally around a point
+      p(0, -1) -> "rotate(90, #{cx}, #{cy})"
+      p(0, 1) -> "rotate(-90, #{cx}, #{cy})"
+      _ -> ""
     end
   end
 end
