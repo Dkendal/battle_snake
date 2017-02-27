2eaa7cf1c8061b1a74dcf56ed589636f293ba23d
Dylan Kendal <dylankendal@gmail.com>
2017-02-27 12:12:40 -0800

    Fix turn speed
    Previously turn delay was additive to the wall clock on handle_info :tick
    
    this makes it act as a lower bound.
    
    e.g
    delay := 100
    wall clock on handle_info :tick := 200
    next tick would be 300 after the start of the last tick
    
    delay := 300
    wall clock on handle_info :tick := 200
    next tick would be 500 after the start of the last tick
    
    After change
    
    delay := 100
    wall clock on handle_info :tick := 200
    next tick would be 200 after the start of the last tick
    
    delay := 300
    wall clock on handle_info :tick := 200
    next tick would be 300 after the start of the last tick

--- test/battle_snake/game_server/server_test.exs
+++ test/battle_snake/game_server/server_test.exs
@@ -1,7 +1,7 @@
 defmodule BattleSnake.GameServer.ServerTest do
   alias BattleSnake.GameServer.Server
   alias BattleSnake.GameServer.State
-  use BattleSnake.Case, async: true
+  use BattleSnake.Case, async: false
 
   def create_game_form(_) do
     [game_form: create(:game_form)]
@@ -40,6 +40,28 @@ defmodule BattleSnake.GameServer.ServerTest do
   describe "Server.handle_call(:get_game_state, _, _)" do
     test "returns the state" do
       assert Server.handle_call(:get_game_state, self(), 1) == {:reply, 1, 1}
+    end
+  end
+
+  describe "Server.handle_info(:tick, state) when state.status is cont" do
+    test "sends a :tick message after the confiured delay as a lower bound" do
+      objective = fn _ -> false end
+      state = build(:state, status: :cont, delay: 2, objective: objective)
+
+      Server.handle_info(:tick, state)
+      assert_receive :tick, 10
+    end
+
+    test "sends a :tick message after execution as an upper bound" do
+      objective = fn _ ->
+        Process.sleep(2)
+        false
+      end
+
+      state = build(:state, status: :cont, delay: 0, objective: objective)
+
+      Server.handle_info(:tick, state)
+      assert_receive :tick, 10
     end
   end
 end
