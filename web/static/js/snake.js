7221deb2a4778cae3edee1dc99a094a51791c59a
Noel Burton-Krahn <noel@burton-krahn.com>
2017-02-28 19:03:14 -0800

    handle empty colors

--- web/static/js/snake.js
+++ web/static/js/snake.js
@@ -672,7 +672,7 @@ function SnakeBoardRenderer(game_renderer) {
         if( !('img' in snake) ) {
             snake.img = snake.head_url || SNAKE_HEAD_URL;
         }
-        if( !('color' in snake) ) {
+        if( !('color' in snake) || !snake.color ) {
             snake.color = stringToColor(snake.board_id);
         }
         if( !('body' in snake) ) {
