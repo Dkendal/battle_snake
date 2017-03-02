286ba90077d31160f96ab86a966166a28bc02cb2
Dylan Kendal <dylan@stembolt.com>
2017-03-01 23:41:24 -0800

    Fix the name of the pixel head

--- lib/snake_heads.ex
+++ lib/snake_heads.ex
@@ -1,5 +1,5 @@
 defmodule BattleSnake.SnakeHeads do
   def list do
-    ~w(bendr dead fang pxl regular safe sand-worm shades smile tongue)
+    ~w(bendr dead fang pixel regular safe sand-worm shades smile tongue)
   end
 end
