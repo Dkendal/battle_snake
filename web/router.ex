a2aa89ba7bfca1213e6cb4cb8599d67215966e64
Clarke Brunsdon <clarke@stembolt.com>
2017-03-02 16:41:38 -0800

    Remove security headers
    Sure, these *sound* like a good idea, but then someone else wants to jam
    your app into an iframe and pretend nothing is amiss and your life
    becomes sad.

--- web/router.ex
+++ web/router.ex
@@ -5,8 +5,6 @@ defmodule BattleSnake.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
   end
 
   pipeline :api do
