bf7ba50394398dc9781c3fc7079903e99e260179
Dylan Kendal <dylan@stembolt.com>
2017-03-02 22:58:50 -0800

    Switch play view over to replay mode using params
    When is_replay is passed as a query param the specator.js is not used.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,11 +1,18 @@
 import "phoenix_html"
 import $ from "jquery";
 import * as BS from "./battle_snake"
+(() => {
+  if (!$("#board-viewer").length) {
+    return;
+  }
 
-const BattleSnake = Object.assign(window.BattleSnake, BS);
-window.BattleSnake = BattleSnake;
+  const BattleSnake = Object.assign(window.BattleSnake, BS);
+  window.BattleSnake = BattleSnake;
 
-if ($("#board-viewer").length) {
-  const gameId = window.BattleSnake.gameId;
-  window.BattleSnake.Spectator.init(gameId);
-}
+  if (window.BattleSnake.isReplay) {
+  }
+  else{
+    const gameId = window.BattleSnake.gameId;
+    window.BattleSnake.Spectator.init(gameId);
+  }
+})();
