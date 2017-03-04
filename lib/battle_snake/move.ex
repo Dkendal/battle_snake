dbd5e9eb0eaf985aacc8c40ec423247b1c849467
Dylan Kendal <dylankendal@gmail.com>
2017-03-03 20:58:10 -0800

    Default moves
    changes the behaviour for default movement from "always go up" to repeat your
    last move.

--- lib/battle_snake/move.ex
+++ lib/battle_snake/move.ex
@@ -15,20 +15,42 @@ defmodule BattleSnake.Move do
     snake_id: reference
   }
 
+  @up %Point{x: 0, y: -1}
   @spec up() :: Point.t
-  def up, do: %Point{x: 0, y: -1}
+  def up, do: @up
 
+  @down %Point{x: 0, y: 1}
   @spec down() :: Point.t
-  def down, do: %Point{x: 0, y: 1}
+  def down, do: @down
 
+  @right %Point{x: 1, y: 0}
   @spec right() :: Point.t
-  def right, do: %Point{x: 1, y: 0}
+  def right, do: @right
 
+  @left %Point{x: -1, y: 0}
   @spec left() :: Point.t
-  def left, do: %Point{x: -1, y: 0}
+  def left, do: @left
 
   @spec default_move() :: Point.t
   def default_move(), do: %Move{move: "up"}
+
+  def default_move(snake) do
+    do_default_move(snake.coords)
+  end
+
+  defp do_default_move([v1, v2 | _]),
+    do: Point.sub(v1, v2)
+    |> from_point
+    |> do_default_move
+
+  defp do_default_move({:ok, move}),
+    do: move
+
+  defp do_default_move(:error),
+    do: default_move()
+
+  defp do_default_move(_),
+    do: do_default_move(:error)
 
   @spec moves() :: [Point.t]
   def moves do
@@ -51,6 +73,21 @@ defmodule BattleSnake.Move do
       "down" -> down()
       "left" -> left()
       "right" -> right()
+    end
+  end
+
+  def from_point(point) do
+    case point do
+      @up ->
+        {:ok, %Move{move: "up"}}
+      @down ->
+        {:ok, %Move{move: "down"}}
+      @left ->
+        {:ok, %Move{move: "left"}}
+      @right ->
+        {:ok, %Move{move: "right"}}
+      _ ->
+        :error
     end
   end
 end
