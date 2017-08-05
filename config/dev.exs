e3f70bdc85eebe293db30935c340684d644f7fbd
Dylan Kendal <dylan.kendal@shopify.com>
2017-08-05 13:47:27 -0400

    Setup scss extension resolution

--- config/dev.exs
+++ config/dev.exs
@@ -12,8 +12,14 @@ config :battle_snake, BattleSnakeWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/.bin/webpack", "--watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: [
+    node: [
+      "node_modules/.bin/webpack",
+      "--watch",
+      "--stdin",
+      "--verbose",
+      "--colors",
+      cd: Path.expand("../assets", __DIR__)]]
 
 
 # Watch static and templates for browser reloading.
