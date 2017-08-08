7a1a0e29b67fbc8ee0fd9a23a97bf9af244ca8da
Dylan Kendal <dylan.kendal@shopify.com>
2017-08-08 19:07:55 -0400

    Add types

---
+++ assets/js/types.ts
@@ -0,0 +1,31 @@
+declare module bs {
+  export type Point = [number, number];
+
+  export type Food = Point;
+
+  export interface Snake {
+    taunt?: any;
+    name: string;
+    id: string;
+    healthPoints: number;
+    headType: string;
+    tailType: string;
+    coords: Point[];
+    color: string;
+    causeOfDeath: string;
+  }
+
+  export interface Board {
+    width: number;
+    turn: number;
+    snakes: Snake[];
+    height: number;
+    gameId: string;
+    food: Food[];
+    deadSnakes: any[];
+  }
+
+  export interface TickResponse {
+    content: Board;
+  }
+}
