8619c2781b412f6fac5d5758939ca0ca3ea3b997
Dylan Kendal <dylan.kendal@shopify.com>
2017-09-24 17:09:06 -0400

    Split up utils

---
+++ assets/src/utils/collections.ts
@@ -0,0 +1,13 @@
+export function* entries<T>(list: bs.List<T>) {
+  for (let i = 0; i < list.length; i++) {
+    yield list.item(i);
+  }
+}
+
+export function map<T, K>(iter: IterableIterator<T>, mapper: (x: T) => K): K[] {
+  return Array.from(iter, mapper);
+}
+
+export function array<T>(iter: IterableIterator<T>): T[] {
+  return map(iter, x => x);
+}
