f8a8f2cb789222ee633ff8d714210fbe4fc64b0f
Dylan Kendal <dylan.kendal@shopify.com>
2017-11-01 18:16:32 -0400

    Split out test.ex into other files.

---
+++ lib/bs/test/vector.ex
@@ -0,0 +1,26 @@
+defmodule Bs.Test.Vector do
+  alias Bs.Point
+
+  use Ecto.Schema
+
+  @primary_key false
+
+  embedded_schema do
+    field(:x, :integer)
+    field(:y, :integer)
+  end
+
+  def to_point(vector) do
+    %Point{
+      x: vector.x,
+      y: vector.y
+    }
+  end
+
+  defmacro v(x, y) do
+    quote bind_quoted: [x: x, y: y], do: %Bs.Test.Vector{
+      x: x,
+      y: y
+    }
+  end
+end
