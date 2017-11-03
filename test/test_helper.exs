4efb8ad106366f54a0a801af780f15fa40a6d12a
Dylan Kendal <dylan.kendal@shopify.com>
2017-11-02 21:55:04 -0400

    Suppress logging in tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,5 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 
-ExUnit.start(exclude: [:skip, :pending], include: :focus)
+ExUnit.start(exclude: [:skip, :pending], include: :focus, capture_log: true)
 
 ExUnit.plural_rule("property", "properties")
