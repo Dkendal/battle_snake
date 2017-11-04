631d656ade54536c9788601a9762c7e73b1109ea
Dylan Kendal <dylan.kendal@shopify.com>
2017-11-04 19:11:20 -0400

    Reorder Bs.Test

--- lib/bs/test.ex
+++ lib/bs/test.ex
@@ -1,166 +1,76 @@
-defmodule Bs.Test.AssertionError do
-  defstruct [
-    :id,
-    :scenario,
-    :world,
-    :player,
-    :reason
-  ]
-end
-
-defmodule Bs.Test.ConnectionError do
-  defstruct [
-    :reason
-  ]
-end
-
 defmodule Bs.Test do
   alias Bs.Movement.Worker
-  alias Bs.Test.AssertionError
-  alias Bs.Test.ConnectionError
   alias Bs.Test.Scenario
   alias Bs.Test.Vector, as: V
   alias Bs.World
-
+  import Bs.Test.Agent, only: :macros
+  import Bs.Test.Vector, only: :macros
   require Bs.Test.Agent
   require Bs.Test.Vector
 
-  import Bs.Test.Vector, only: :macros
-  import Bs.Test.Agent, only: :macros
+  @moduledoc ~S"""
+  Continuous Integration module. This module is responsible for defining test
+  cases, and running them against a client.
+  """
 
-  def transform(model, f, dim \\ {:error, :error})
+  # H 0
+  # 0 0
+  @near_wall %Scenario{
+    width: 2,
+    height: 2,
+    player: agent([[0, 0] * 3]),
+    agents: [],
+    food: []
+  }
 
-  def transform(list, f, dim) when is_list(list) do
-    for x <- list, do: transform(x, f, dim)
-  end
+  # H T
+  # 0 0
+  @only_one_move %Scenario{
+    width: 2,
+    height: 2,
+    player: agent([[0, 0], [1, 0] * 2]),
+    agents: [],
+    food: []
+  }
 
-  def transform(%Scenario{width: width, height: height} = model, f, _dim) do
-    model
-    |> Map.from_struct()
-    |> Enum.reduce(model, fn {k, v}, s ->
-         %{s | k => transform(v, f, {width, height})}
-       end)
-  end
+  # X Y
+  # 0 0
+  @near_opponent %Scenario{
+    width: 2,
+    height: 2,
+    player: agent([[1, 0] * 2]),
+    agents: [agent([[0, 0] * 3])],
+    food: []
+  }
 
-  def transform(%V{x: x, y: y}, f, {w, h}) do
-    apply(f, [{w, h}, {x, y}])
-  end
+  # X Y
+  @head_on %Scenario{
+    width: 2,
+    height: 1,
+    player: agent([[1, 0] * 3]),
+    agents: [agent([[0, 0] * 2])],
+    food: []
+  }
 
-  def transform(%{__struct__: _} = model, f, dim) do
-    model
-    |> Map.from_struct()
-    |> Enum.reduce(model, fn {k, v}, s ->
-         %{s | k => transform(v, f, dim)}
-       end)
-  end
+  defmodule(
+    AssertionError,
+    do: defstruct([
+      :id,
+      :scenario,
+      :world,
+      :player,
+      :reason
+    ])
+  )
 
-  def transform(model, _, _) do
-    model
-  end
+  defmodule(
+    ConnectionError,
+    do: defstruct([
+      :reason
+    ])
+  )
 
-  def scenarios do
-    s = [
-      # H 0
-      # 0 0
-      %Scenario{
-        width: 2,
-        height: 2,
-        player: agent([[0, 0] * 3]),
-        agents: [],
-        food: []
-      },
-      # H T
-      # 0 0
-      %Scenario{
-        width: 2,
-        height: 2,
-        player: agent([[0, 0], [1, 0] * 2]),
-        agents: [],
-        food: []
-      },
-      # X Y
-      # 0 0
-      %Scenario{
-        width: 2,
-        height: 2,
-        player: agent([[1, 0] * 2]),
-        agents: [agent([[0, 0] * 3])],
-        food: []
-      }
-    ]
-
-    s =
-      transform(s, fn {_w, _h}, {x, y} ->
-        v(y, x)
-      end)
-
-    s =
-      transform(s, fn {_w, h}, {x, y} ->
-        v(x, h - y - 1)
-      end) ++ s
-
-    s = [
-      # X Y
-      %Scenario{
-        width: 2,
-        height: 1,
-        player: agent([[1, 0] * 3]),
-        agents: [agent([[0, 0] * 2])],
-        food: []
-      }
-      | s
-    ]
-
-    s =
-      transform(s, fn {w, _h}, {x, y} ->
-        v(w - x - 1, y)
-      end) ++ s
-
-    s
-    |> List.flatten()
-    |> Enum.uniq()
-  end
-
-  def start(scenarios, url) do
-    {:ok, sup} = Task.Supervisor.start_link()
-
-    sup
-    |> Task.Supervisor.async_stream_nolink(scenarios, fn scenario ->
-         test(scenario, url)
-       end)
-    |> Stream.map(fn
-         {:ok, result} ->
-           result
-
-         {:exit, {%{reason: reason}, _stack}} ->
-           %ConnectionError{reason: reason}
-       end)
-    |> Enum.to_list()
-  end
-
-  def generate_ids(model) when is_map(model) do
-    model =
-      if Map.has_key?(model, :id) do
-        %{model | id: Ecto.UUID.generate()}
-      else
-        model
-      end
-
-    model
-    |> Map.from_struct()
-    |> Enum.reduce(model, fn {k, v}, model ->
-         %{model | k => generate_ids(v)}
-       end)
-  end
-
-  def generate_ids(model) when is_list(model) do
-    Enum.map(model, &generate_ids/1)
-  end
-
-  def generate_ids(model) do
-    model
-  end
-
+  @doc ~S"Run a scenario, returns ok if it passes, or a test case error."
   def test(scenario, url, move_fun \\ &Worker.run/3) do
     scenario = generate_ids(scenario)
 
@@ -189,5 +99,105 @@ defmodule Bs.Test do
         world: world
       }
     end
+  end
+
+  def scenarios do
+    s = [
+      @near_wall,
+      @only_one_move,
+      @near_opponent
+    ]
+
+    # swap x and y
+    s =
+      transform(s, fn {_w, _h}, {x, y} ->
+        v(y, x)
+      end)
+
+    s = [@head_on | s]
+
+    # flip y
+    s =
+      transform(s, fn {_w, h}, {x, y} ->
+        v(x, h - y - 1)
+      end) ++ s
+
+    # flip x
+    s =
+      transform(s, fn {w, _h}, {x, y} ->
+        v(w - x - 1, y)
+      end) ++ s
+
+    s
+    |> List.flatten()
+    |> Enum.uniq()
+  end
+
+  def start(scenarios, url) do
+    {:ok, sup} = Task.Supervisor.start_link()
+
+    sup
+    |> Task.Supervisor.async_stream_nolink(scenarios, __MODULE__, :test, [url])
+    |> Stream.map(fn
+         {:ok, result} ->
+           result
+
+         {:exit, {%{reason: reason}, _stack}} ->
+           %ConnectionError{reason: reason}
+       end)
+    |> Enum.to_list()
+  end
+
+  defp transform(model, f, dim \\ {:error, :error})
+
+  defp transform(list, f, dim) when is_list(list) do
+    for x <- list, do: transform(x, f, dim)
+  end
+
+  defp transform(%Scenario{width: width, height: height} = model, f, _dim) do
+    model
+    |> Map.from_struct()
+    |> Enum.reduce(model, fn {k, v}, s ->
+         %{s | k => transform(v, f, {width, height})}
+       end)
+  end
+
+  defp transform(%V{x: x, y: y}, f, {w, h}) do
+    apply(f, [{w, h}, {x, y}])
+  end
+
+  defp transform(%{__struct__: _} = model, f, dim) do
+    model
+    |> Map.from_struct()
+    |> Enum.reduce(model, fn {k, v}, s ->
+         %{s | k => transform(v, f, dim)}
+       end)
+  end
+
+  defp transform(model, _, _) do
+    model
+  end
+
+  defp generate_ids(model) when is_map(model) do
+    model =
+      if Map.has_key?(model, :id) do
+        %{model | id: Ecto.UUID.generate()}
+      else
+        model
+      end
+
+    model
+    |> Map.from_struct()
+    |> Enum.reduce(model, fn {k, v}, model ->
+         %{model | k => generate_ids(v)}
+       end)
+  end
+
+  defp generate_ids(model) when is_list(model) do
+    Enum.map(model, &generate_ids/1)
+  end
+
+  defp generate_ids(model) do
+    model
   end
 end
