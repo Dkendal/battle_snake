d1530976fce4b01266407e464508bdf4fdcc396c
Dylan Kendal <dylan.kendal@shopify.com>
2018-01-14 21:09:22 -0500

    Redraw on resize

--- assets/src/app/Game.ts
+++ assets/src/app/Game.ts
@@ -1,9 +1,12 @@
-import './Game.css';
 import 'phoenix_html';
+
+import './Game.css';
+
 import {Game} from 'elm/Game';
-import {embedApp} from '../utils';
-import {GameBoard} from '../GameBoard';
+
 import css from '../css-variables';
+import {GameBoard} from '../GameBoard';
+import {embedApp} from '../utils';
 
 const colorPallet = new Map<string, string>(Object.entries(css));
 
@@ -12,24 +15,32 @@ document.addEventListener('DOMContentLoaded', () => {
     websocket: `ws://${window.location.host}/socket/websocket`,
   };
 
-  embedApp('Game', Game, gameAppConfig).map(program => {
-    let board: GameBoard;
+  embedApp('Game', Game, gameAppConfig).map((program: any) => {
+    let gameBoard: GameBoard;
+    let gameState: GameState;
 
-    program.ports.render.subscribe(({board: board_}) => {
-      console.debug('render', board_)
-      if (!board) {
-        const id = board_.gameId;
+    window.addEventListener('resize', () => {
+      requestAnimationFrame(() => {
+        gameBoard.draw(gameState.board);
+      });
+    });
+
+    program.ports.render.subscribe(({content}: {content: GameState}) => {
+      gameState = content;
+
+      if (!gameBoard) {
+        const id = gameState.board.gameId;
         const node = document.getElementById(id);
 
         if (!node) {
           throw new Error(`could not find an element with id ${id}`);
         }
 
-        board = new GameBoard(node, colorPallet);
+        gameBoard = new GameBoard(node, colorPallet);
       }
 
       requestAnimationFrame(() => {
-        board.draw(board_);
+        gameBoard.draw(gameState.board);
       });
     });
   });
