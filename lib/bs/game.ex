53f7ebadb8f88ffea7b16dda30e76c90b2e51ee3
Dylan Kendal <dylan.kendal@shopify.com>
2018-01-17 20:41:25 -0500

    Remove unused method

--- lib/bs/game.ex
+++ lib/bs/game.ex
@@ -28,12 +28,6 @@ defmodule Bs.Game do
     id |> do_ensure_started |> call(:get_game_state)
   end
 
-  def get_state_async(id, callback) when is_binary(id) do
-    Elixir.Registry.dispatch(Game.Registry, id, fn entries ->
-      nil
-    end)
-  end
-
   def next(id) when is_binary(id) do
     id |> do_ensure_started |> call(:next)
   end
