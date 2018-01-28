def3cdbafc778c390c5ee4572ab1def1060d3682
Dylan Kendal <dylankendal@gmail.com>
2018-01-28 15:17:39 -0500

    Update game channel test

--- test/bs_web/channels/game_channel_test.exs
+++ test/bs_web/channels/game_channel_test.exs
@@ -3,39 +3,35 @@ defmodule BsWeb.Channels.GameChannelTest do
 
   use BsWeb.ChannelCase
 
-  test "pushes the game state after joining" do
-    id =
-      insert(
-        :game_form,
-        snakes: [
-          build(:snake_form),
-          build(:snake_form)
-        ]
-      ).id
+  test "can control stepping back and forth" do
+    game_form =
+      insert(:game_form, snakes: [build(:snake_form), build(:snake_form)])
+
+    id = game_form.id
 
     sock = socket()
 
     {:ok, _, sock} = subscribe_and_join(sock, GameChannel, "game:#{id}")
 
-    assert_broadcast("tick", %{
-      content: %{
-        status: _,
-        board: %{
-          id: _,
-          gameId: _,
-          food: _,
-          turn: 0,
-          snakes: snakes,
-          deadSnakes: deadSnakes,
-          width: _,
-          height: _
-        }
-      }
-    })
+    assert_broadcast("tick", msg)
 
-    assert 2 == length(snakes)
-    assert 0 == length(deadSnakes)
+    assert msg.content.status == :suspend
+    assert msg.content.board.turn == 0
+    assert 2 == length(msg.content.board.snakes)
+    assert 0 == length(msg.content.board.deadSnakes)
 
-    push(sock, "start")
+    push(sock, "next")
+
+    assert_broadcast("tick", msg)
+
+    assert msg.content.status == :suspend
+    assert msg.content.board.turn == 1
+
+    push(sock, "prev")
+
+    assert_broadcast("tick", msg)
+
+    assert msg.content.status == :suspend
+    assert msg.content.board.turn == 0
   end
 end
